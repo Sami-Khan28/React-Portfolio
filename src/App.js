19d59ba1319b6e7200b86c6f1dcfd66923efa9ad
Sumiaya <Sumaiya245>
2025-07-08 21:00:55 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
     <>
       <GlassNavbar />
       <AnimatedBackground />
+       
       <section id="home"><Header /></section>
       <section id="about"><About /></section>
       <section id="projects"><Projects /></section>
       <section id="contact"><Contact /></section>
+      <Footer />
     </>
   );
 }
