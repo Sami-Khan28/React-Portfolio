ab314e9d42f1a332556be43883aae96905bc2f0d
Sumiaya <Sumaiya245>
2025-07-08 20:46:00 +0530

    proper linking

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
             <h4 className="glass-title">Portfolio Website</h4>
             <p>A personal portfolio built using React, Bootstrap.</p>
             <a
-              href="https://sami-portfolio-inky.vercel.app/"
+              href="https://sami-portfoli0.netlify.app/"
               target="_blank"
               rel="noreferrer"
               className="glass-button"
